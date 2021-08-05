a0cc72873a8b6f5075e1e52846f59ecd973c2015
Ethan Charoenpitaks <echaroenpitaks@gmail.com>
2021-08-05 16:03:29 -0500

    Added generic Page template.

---
+++ template_Page.py
@@ -0,0 +1,26 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+import subprocess
+
+import LXSettings
+
+class __page_PAGE_PATH_IDENTIFIER(LXSettings.XEmbedPage):
+    def __init__(self):
+        super().init_begin()
+
+        self.page_path_str = "PAGE_PATH_STR"
+        self.page_name = "PAGE_NAME_STR"
+        self.icon_name = "PAGE_ICON_NAME"
+        
+        super().init_end()
+    
+    def on_enter(self):
+        super().on_enter_begin()
+
+        
+
+        super().on_enter_end()
+
+page_PAGE_PATH_IDENTIFIER = __page_PAGE_PATH_IDENTIFIER()
